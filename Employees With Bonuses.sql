b31c148fb2f291531eb202dbb7b703303a25e78b
umaraj033107 <umaraj03073110@gmail.com>
2022-11-03 15:44:41 -0700

    Solution to Employees with Bonuses

---
+++ Employees With Bonuses.sql
@@ -0,0 +1,6 @@
+/*Employees With Bonuses*/
+/*Question: https://platform.stratascratch.com/coding/9903-employees-with-bonuses?code_type=3
+*/
+select first_name, bonus
+from employee
+where bonus<150;
