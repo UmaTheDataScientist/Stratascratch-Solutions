b9c6412fca0e0a474320f34759142f2a1c417d1f
umaraj033107 <umaraj03073110@gmail.com>
2022-11-21 22:59:51 -0800

    Create Employees With Bonuses.py

---
+++ Employees With Bonuses.py
@@ -0,0 +1,14 @@
+/*
+Employees With Bonuses
+
+
+https://platform.stratascratch.com/coding/9903-employees-with-bonuses?code_type=2
+*/
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+employee.head()
+
+employee[['first_name','bonus']][employee['bonus']<150]
